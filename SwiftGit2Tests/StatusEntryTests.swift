7e4db78f4edbd4901f27665153804e6639d9dced
UKS <skulptorr@gmail.com>
2021-11-15 17:04:38 +0200

    tests upd

--- SwiftGit2Tests/StatusEntryTests.swift
+++ SwiftGit2Tests/StatusEntryTests.swift
@@ -5,9 +5,18 @@ import EssetialTesting
 
 class StatusEntryTests: XCTestCase {
     func testBla() {
-        GitTest.tmpURL
-            .flatMap { Repository.create(at: $0) }
-            .shouldSucceed()
+        let repo = Repository.t_randomRepo()
+        _ = repo.flatMap { $0.t_write(file: .fileA, with: .random) }
+        
+        
+        let options = StatusOptions(flags: [.includeUntracked, .renamesHeadToIndex])
+        let status = repo.flatMap { $0.status(options: options) }
+            .shouldSucceed()!
+        
+        let entrie = status[0]
+        XCTAssert(entrie.indexToWorkDir != nil)
+        XCTAssert(entrie.headToIndex == nil)
+        
     }
 
 }
