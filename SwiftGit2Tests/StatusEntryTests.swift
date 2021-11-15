f1834a4db143004b968c58dace304ff51063178e
UKS <skulptorr@gmail.com>
2021-11-15 21:40:16 +0200

    test_should_return_EntyFileInfo_Commit_rename() failing

--- SwiftGit2Tests/StatusEntryTests.swift
+++ SwiftGit2Tests/StatusEntryTests.swift
@@ -65,7 +65,7 @@ class StatusEntryTests: XCTestCase {
     }
     
     func test_should_return_EntyFileInfo_Commit_rename() {
-        let url = URL.randomTempDirectory().maybeSuccess!
+        let url = URL(fileURLWithPath: NSTemporaryDirectory().appending(UUID().uuidString), isDirectory: true)
         
         print("REPO_URL \(url.path)")
         
@@ -74,9 +74,10 @@ class StatusEntryTests: XCTestCase {
             .shouldSucceed()
         
         url.moveFile(at: TestFile.fileA.rawValue, to: TestFile.fileB.rawValue)
-        //TestFile.fileB.rawValue
+        
         Repository.at(url: url)
-            .flatMap { $0.addAllFiles() }
+            .flatMap { $0.addBy(path: TestFile.fileB.rawValue) }
+            .flatMap { $0.addBy(path: TestFile.fileA.rawValue) }
             .flatMap { $0.commit(message: "rename", signature: GitTest.signature) }
             .shouldSucceed()
         
