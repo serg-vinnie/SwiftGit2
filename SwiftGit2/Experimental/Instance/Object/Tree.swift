0c95fd8e29312104eb563f0fc96895d481b039ff
UKS <skulptorrr@gmail.com>
2021-12-05 11:16:45 +0200

    test code added

--- SwiftGit2/Experimental/Instance/Object/Tree.swift
+++ SwiftGit2/Experimental/Instance/Object/Tree.swift
@@ -25,6 +25,29 @@ public extension Tree {
     var oid : OID { OID(git_tree_id(self.pointer).pointee) }
 }
 
+public extension Tree {
+//    func blobLookup(byPath: String) -> R<Blob>{
+//        var pointer: OpaquePointer?
+//
+//        return _result( { Blob(pointer!) }, pointOfFailure: "git_object_lookup_bypath") {
+//            byPath.withCString { path in
+//                git_object_lookup_bypath(&pointer, self.pointer, path, git_object_t(rawValue: 3) ) // raw 3 means blob
+//            }
+//        }
+//
+//    }
+    
+//    func entry(byPath: String) -> R<Blob>{
+//        var pointer: OpaquePointer?
+//        
+//        return _result( { RepoTreeEntry(pointer!) }, pointOfFailure: "git_object_lookup_bypath") {
+//            byPath.withCString { path in
+//                git_tree_entry_bypath(&pointer, self.pointer, path, git_object_t(rawValue: 3) ) // raw 3 means blob
+//            }
+//        }
+//    }
+}
+
 public extension Repository {
     func diffTreeToTree(oldTree: Tree?, newTree: Tree?, options: DiffOptions = DiffOptions()) -> Result<Diff, Error> {
         var diff: OpaquePointer?
@@ -61,4 +84,6 @@ public extension Repository {
 
         return .success(Diff(diff!))
     }
+    
+    
 }
