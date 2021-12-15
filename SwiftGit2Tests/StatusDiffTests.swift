e8d395cd4eedbee53f42312e0f1c86b8d4fe37c3
UKS <skulptorr@gmail.com>
2021-12-15 16:02:08 +0200

    StatusDiffTests wip

--- SwiftGit2Tests/StatusDiffTests.swift
+++ SwiftGit2Tests/StatusDiffTests.swift
@@ -4,6 +4,18 @@ import XCTest
 import EssetialTesting
 
 class StatusDiffTests: XCTestCase {
+    let urlRoot = URL.userHome.appendingPathComponent(".TaoTestData")
+    lazy var urlHeadIsUnborn = urlRoot.appendingPathComponent("dst/headIsUnborn")
+    lazy var urlOneCommit = urlRoot.appendingPathComponent("dst/oneCommit")
+    
+    override func setUp() {
+        let dstURL = urlRoot.appendingPathComponent("dst")
+        _ = dstURL.rm()
+        let srcURL = urlRoot.appendingPathComponent("src")
+        srcURL.copy(to: dstURL)
+            .shouldSucceed()
+    }
+    
     func test_should_return_content_of_Untracked_Unstaged_File() {
         
     }
