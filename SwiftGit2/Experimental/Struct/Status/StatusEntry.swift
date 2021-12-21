a2dd319366eec98b9e1d25738fb461b7774b39f1
UKS <skulptorrr@gmai.com>
2021-12-21 18:03:21 +0200

    UiStatusEntryX separated

--- SwiftGit2/Experimental/Struct/Status/StatusEntry.swift
+++ SwiftGit2/Experimental/Struct/Status/StatusEntry.swift
@@ -25,34 +25,6 @@ public struct StatusEntry {
 }
 
 public extension StatusEntry {
-    struct Status: OptionSet {
-        // This appears to be necessary due to bug in Swift
-        // https://bugs.swift.org/browse/SR-3003
-        public init(rawValue: UInt32) {
-            self.rawValue = rawValue
-        }
-        
-
-        public let rawValue: UInt32
-
-        public static let current = Status(rawValue: GIT_STATUS_CURRENT.rawValue)
-        public static let indexNew = Status(rawValue: GIT_STATUS_INDEX_NEW.rawValue)
-        public static let indexModified = Status(rawValue: GIT_STATUS_INDEX_MODIFIED.rawValue)
-        public static let indexDeleted = Status(rawValue: GIT_STATUS_INDEX_DELETED.rawValue)
-        public static let indexRenamed = Status(rawValue: GIT_STATUS_INDEX_RENAMED.rawValue)
-        public static let indexTypeChange = Status(rawValue: GIT_STATUS_INDEX_TYPECHANGE.rawValue)
-        public static let workTreeNew = Status(rawValue: GIT_STATUS_WT_NEW.rawValue)
-        public static let workTreeModified = Status(rawValue: GIT_STATUS_WT_MODIFIED.rawValue)
-        public static let workTreeDeleted = Status(rawValue: GIT_STATUS_WT_DELETED.rawValue)
-        public static let workTreeTypeChange = Status(rawValue: GIT_STATUS_WT_TYPECHANGE.rawValue)
-        public static let workTreeRenamed = Status(rawValue: GIT_STATUS_WT_RENAMED.rawValue)
-        public static let workTreeUnreadable = Status(rawValue: GIT_STATUS_WT_UNREADABLE.rawValue)
-        public static let ignored = Status(rawValue: GIT_STATUS_IGNORED.rawValue)
-        public static let conflicted = Status(rawValue: GIT_STATUS_CONFLICTED.rawValue)
-    }
-}
-
-public extension StatusEntry {
     var headToIndexNEWFilePath : R<String> {
         self.headToIndex.asNonOptional("headToIndex_newFilePath") | { $0.newFilePath }
     }
---
+++ SwiftGit2/Experimental/Struct/Status/UiStatusEntryX.swift
@@ -0,0 +1,174 @@
+import Foundation
+import Essentials
+
+/////////////////////////////////
+// NEW STATUS ENTRY
+/////////////////////////////////
+
+public protocol UiStatusEntryX {
+    var stageState: StageState { get }
+    var stagedPatch: Result<Patch?, Error> { get }
+    var unstagedPatch: Result<Patch?, Error> { get }
+    
+    ///indexToWorkDir
+    var unStagedDeltas: Diff.Delta? { get }
+    
+    ///headToIndex
+    var stagedDeltas: Diff.Delta? { get }
+    var changesDeltas: Diff.Delta? { get }
+    
+    var oldFileRelPath: String? { get }
+    var newFileRelPath: String? { get }
+    
+    var status: StatusEntry.Status { get }
+    
+    func statusFull() -> [Diff.Delta.Status]
+    
+    //var isBinary: Bool? { get }
+}
+
+public extension StatusEntry {
+    func asStatusEntryX(repo: Repository) -> UiStatusEntryX {
+        let entry = self
+        
+        var stagedPatch: R<Patch?>
+        var unStagedPatch: R<Patch?>
+        
+        if let hti = entry.headToIndex {
+            stagedPatch = repo.patchFrom(delta: hti)
+                .map{ patch -> Patch? in patch }
+        } else {
+            stagedPatch = .success(nil)
+        }
+        
+        if let itw =  entry.indexToWorkDir {
+            unStagedPatch = repo.patchFrom(delta: itw)
+                .map{ patch -> Patch? in patch }
+        } else {
+            unStagedPatch = .success(nil)
+        }
+        
+        switch getChanged(repo: repo) {
+        case let .success(changesDelta):
+            return StatusEntryNew(entry, stagedPatch: stagedPatch, unStagedPatch: unStagedPatch, changesDeltas: changesDelta)
+        case let .failure(error):
+            print("StatusEntry.asStatusEntryX ERROR: \(error)")
+            return StatusEntryNew(entry, stagedPatch: stagedPatch, unStagedPatch: unStagedPatch, changesDeltas: nil)
+        }
+    }
+    
+    fileprivate func getChanged(repo: Repository) -> R<[Diff.Delta]> {
+        if let _ = repo.submoduleLookup(named: self.relPath).maybeSuccess {
+            return .success([])
+        }
+        
+        if self.statuses.contains(.added) || self.statuses.contains(.untracked) {
+            return .success([])
+        }
+        
+        // we don't need to detect renames in this case
+        let headBlob = repo.deltas(target: .HEADorWorkDir, findOptions: Diff.FindOptions())
+            .flatMap { $0.deltasWithHunks.fileOid(path: self.relPath) }
+            .flatMap { repo.blob(oid: $0) }
+        
+        return combine(headBlob, repo.blobCreateFromWorkdirAsBlob(relPath: relPath))
+            .flatMap { repo.diffBlobs(old: $0, new: $1) }
+    }
+    
+    fileprivate func anyFilePath() -> Diff.File? {
+        self.headToIndex?.oldFile ??
+        self.indexToWorkDir?.oldFile ??
+        self.headToIndex?.newFile ??
+        self.indexToWorkDir?.newFile
+    }
+}
+
+private struct StatusEntryNew: UiStatusEntryX {
+    private var entry: StatusEntry
+    private var stagedPatch_: Result<Patch?, Error>
+    private var unStagedPatch_: Result<Patch?, Error>
+    
+    init(_ entry: StatusEntry, stagedPatch: Result<Patch?, Error>, unStagedPatch: Result<Patch?, Error>, changesDeltas: [Diff.Delta]?) {
+        self.entry = entry
+        self.stagedPatch_ = stagedPatch
+        self.unStagedPatch_ = unStagedPatch
+        self.changesDeltas = changesDeltas?.first
+        //self.isBinary = isBinary
+    }
+    
+    public var oldFileRelPath: String? { entry.headToIndex?.oldFile?.path ?? entry.indexToWorkDir?.oldFile?.path }
+    
+    public var newFileRelPath: String? { entry.headToIndex?.newFile?.path ?? entry.indexToWorkDir?.newFile?.path }
+    
+    public var stagedPatch: Result<Patch?, Error> { stagedPatch_ }
+    
+    public var unstagedPatch: Result<Patch?, Error> { unStagedPatch_ }
+    
+    var unStagedDeltas: Diff.Delta? { entry.indexToWorkDir }
+    
+    var stagedDeltas: Diff.Delta? { entry.headToIndex }
+    
+    var changesDeltas: Diff.Delta?
+    
+    var isBinary: Bool?
+    
+    var stageState: StageState {
+        if entry.headToIndex != nil && entry.indexToWorkDir != nil {
+            return .mixed
+        }
+        
+        if let _ = entry.headToIndex {
+            return .staged
+        }
+        
+        if let _ = entry.indexToWorkDir {
+            return .unstaged
+        }
+        
+        assert(false)
+        return .mixed
+    }
+    
+    var status: StatusEntry.Status { entry.status }
+    
+    func statusFull() -> [Diff.Delta.Status] {
+        if let status = unStagedDeltas?.status,
+           stagedDeltas == nil {
+                return [status]
+        }
+        if let status = stagedDeltas?.status,
+            unStagedDeltas == nil {
+                return [status]
+        }
+        
+        guard let workDir = unStagedDeltas?.status else { return [.unmodified] }
+        guard let index = stagedDeltas?.status else { return [.unmodified] }
+        
+        if workDir == index {
+            return [workDir]
+        }
+        
+        return [workDir, index]
+    }
+}
+
+
+///////////////////////////////////
+/// HELPERS
+//////////////////////////////////
+fileprivate extension StatusEntry{
+    var relPath: String {
+        self.headToIndex?.newFile?.path     ?? self.indexToWorkDir?.newFile?.path ??
+            self.headToIndex?.oldFile?.path ?? self.indexToWorkDir?.oldFile?.path ?? ""
+    }
+}
+
+private extension Array where Element == Diff.Delta {
+    func fileOid(path: String) -> R<OID> {
+        if let newFile = self.first(where: { $0.newFile?.path == path })?.newFile {
+            return .success(newFile.oid)
+        }
+        
+        return .wtf("can't find fileOid for path: \(path)")
+    }
+}
