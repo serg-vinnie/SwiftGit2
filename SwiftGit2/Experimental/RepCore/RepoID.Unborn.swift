1c198ae4969198c8865a95f79deef946f59368a6
Sergiy <sergiy@vynnychenko.com>
2022-03-08 14:16:54 +0200

    clean up

--- SwiftGit2/Experimental/RepCore/RepoID.Unborn.swift
+++ SwiftGit2/Experimental/RepCore/RepoID.Unborn.swift
@@ -30,23 +30,3 @@ public extension Unborn {
         return clone(from: str, options: options)
     }
 }
-
-
-//// STATIC funcs
-//public extension Repository {
-//    static func clone(from remoteURL: String, to localURL: URL, options: CloneOptions) -> R<Repository> {
-//        git_instance(of: Repository.self, "git_clone") { pointer in
-//            options.with_git_clone_options { clone_options in
-//                localURL.withUnsafeFileSystemRepresentation { destination in
-//                    git_clone(&pointer, remoteURL, destination, &clone_options)
-//                }
-//            }
-//        }
-//    }
-//
-//    
-//    static func clone(from remoteURL: URL, to localURL: URL, options: CloneOptions) -> Result<Repository, Error> {
-//        let remoteURLString = (remoteURL as NSURL).isFileReferenceURL() ? remoteURL.path : remoteURL.absoluteString
-//        return clone(from: remoteURLString, to: localURL, options: options)
-//    }
-//}
