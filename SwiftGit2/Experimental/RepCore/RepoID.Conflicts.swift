39538ab4e6502d9176819f17af346ffdf19fb988
UKS <skulptorr@gmail.com>
2022-03-16 15:11:50 +0200

    commented temp code

--- SwiftGit2/Experimental/RepCore/RepoID.Conflicts.swift
+++ SwiftGit2/Experimental/RepCore/RepoID.Conflicts.swift
@@ -34,41 +34,44 @@ public struct Conflicts {
         let conflict = index | { $0.conflict(path: path) }
         let entry = conflict | { type == .their ? $0.their : $0.our }
         
+        // Видаляємо конфлікт
         index = index | { $0.removeAll(pathPatterns: [path]) }
                       | { $0.conflictRemove(relPath: path) }
         
+        // додаємо файл чи сабмодуль в індекс
         index = combine(index, entry) | { index, entry in index.add(entry) }
         
+        // чекаутим файл чи сабмодуль з цього індекса
         return combine(repo, index) | { repo, index in repo.checkout(index: index, strategy: [.Force, .DontWriteIndex]) } | { _ in () }
     }
     
-    public func resolve(path: String, resolveAsTheir: Bool) -> R<()> {
-        let repo = repoID.repo.maybeSuccess!
-        let index = repo.index()
-        
-        let rConflict: R<Index.Conflict> = all().map{ $0.filter{ $0.our.path == path || $0.their.path == path }.first! }
-        
-        
-        
-        return combine(index, rConflict)
-            .flatMap { index, conflict -> R<()> in
-                let sideEntry = resolveAsTheir ? conflict.their : conflict.our
-                
-                return index
-                    .removeAll(pathPatterns: [sideEntry.path])
-                    // Видаляємо конфлікт
-                    .flatMap{ $0.conflictRemove(relPath: sideEntry.path) }
-                    // додаємо файл чи сабмодуль в індекс
-                    .flatMap {
-                        $0.add(sideEntry)
-                    }
-                    // чекаутим файл чи сабмодуль з цього індекса
-                    .flatMap { _ in
-                        repo.checkout( index: index, strategy: [.Force, .DontWriteIndex])//, relPaths: [sideEntry.path] )
-                    }
-            }
-            .flatMap{ _ in .success(()) }
-    }
+//    public func resolve(path: String, resolveAsTheir: Bool) -> R<()> {
+//        let repo = repoID.repo.maybeSuccess!
+//        let index = repo.index()
+//
+//        let rConflict: R<Index.Conflict> = all().map{ $0.filter{ $0.our.path == path || $0.their.path == path }.first! }
+//
+//
+//
+//        return combine(index, rConflict)
+//            .flatMap { index, conflict -> R<()> in
+//                let sideEntry = resolveAsTheir ? conflict.their : conflict.our
+//
+//                return index
+//                    .removeAll(pathPatterns: [sideEntry.path])
+//                    // Видаляємо конфлікт
+//                    .flatMap{ $0.conflictRemove(relPath: sideEntry.path) }
+//                    // додаємо файл чи сабмодуль в індекс
+//                    .flatMap {
+//                        $0.add(sideEntry)
+//                    }
+//                    // чекаутим файл чи сабмодуль з цього індекса
+//                    .flatMap { _ in
+//                        repo.checkout( index: index, strategy: [.Force, .DontWriteIndex])//, relPaths: [sideEntry.path] )
+//                    }
+//            }
+//            .flatMap{ _ in .success(()) }
+//    }
 }
 
 extension Index {
