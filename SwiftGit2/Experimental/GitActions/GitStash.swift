35237948ca385f5a7649023ed596212d1fe6b2cc
UKS <skulptorrr@gmail.com>
2022-05-15 23:43:14 +0300

    GitStash

---
+++ SwiftGit2/Experimental/GitActions/GitStash.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+public struct GitStash {
+    public let repoID: RepoID
+    
+    func save() {
+        
+    }
+    
+    func apply() {
+        
+    }
+    
+    func all() {
+        
+    }
+    
+    
+    func remove() {
+        //Drop
+    }
+}
