d22a3165b90ce98e0bb0a1588df27afbbfe8f22d
UKS <skulptorrr@gmail.com>
2022-05-16 21:02:56 +0300

    GitStash public

--- SwiftGit2/Experimental/GitActions/GitStash.swift
+++ SwiftGit2/Experimental/GitActions/GitStash.swift
@@ -4,22 +4,26 @@ import Essentials
 public struct GitStash {
     public let repoID: RepoID
     
-    func save(signature: Signature, message: String, flags: StashFlags = .defaultt ) -> R<OID>  {
+    public init(repoID: RepoID) {
+        self.repoID = repoID
+    }
+    
+    public func save(signature: Signature, message: String, flags: StashFlags = .defaultt ) -> R<OID>  {
         return repoID.repo
             .flatMap { $0.stashSave(signature: signature, message: message, flags: flags) }
     }
     
-    func load(_ stash: Stash) -> R<()> {
+    public func load(_ stash: Stash) -> R<()> {
         repoID.repo
             .flatMap { $0.stashLoad(stash) }
     }
     
-    func items() -> R<[Stash]> {
+    public func items() -> R<[Stash]> {
         return repoID.repo
             .flatMap { $0.stashForeach() }
     }
     
-    func remove(_ stash: Stash) -> R<()> {
+    public func remove(_ stash: Stash) -> R<()> {
         return repoID.repo
             .flatMap { $0.stashDrop(stash) }
     }
