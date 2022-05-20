c63bb4ca32c6d8efe5c10afbe86b27103e284147
UKS <skulptorr@gmail.com>
2022-05-20 23:54:20 +0300

    GitCommit+Test  test_revertConflict()

--- SwiftGit2Tests/GitCommit+Test.swift
+++ SwiftGit2Tests/GitCommit+Test.swift
@@ -16,11 +16,27 @@ class GitCommitTests: XCTestCase {
         
         gitCommit.revert(commit: commitToRevert).shouldSucceed()
         
-        _ = src.addAllAndCommit(msg: "reverted to 1").shouldSucceed()!
+        _ = src.addAllAndCommit(msg: "commit 2 is reverted").shouldSucceed()!
         
         let currContent = File(url: src.urlOf(file: .fileA) ).getContent()
         let contentMustBe = TestFileContent.content1.rawValue
         
         XCTAssertEqual(currContent, contentMustBe)
+    }
+    
+    func test_revertConflict() {
+        let src = root.with(repo: "test_revertConflict", content: .commit(.fileA, .content1, "1")).shouldSucceed()!
+        
+        let repoID = RepoID(path: src.url.path )
+        let gitCommit = GitCommit(repoID: repoID)
+        
+        let commitToRevert = src.commit(file: .fileA, with: .content2, msg: "2").shouldSucceed()!
+        _ = src.commit(file: .fileA, with: .content3, msg: "3").shouldSucceed()!
+        
+        _ = gitCommit.revert(commit: commitToRevert).shouldSucceed()
+        
+        GitConflicts(repoID: repoID)
+            .exist()
+            .assertEqual(to: true)
     }
 }
