ed423d2917bb95b4d70102fd1dc1acc011485f4b
UKS <skulptorrr@gmail.com>
2022-07-20 18:33:42 +0300

    FileChangesTests

--- SwiftGit2Tests/FileChangesTests.swift
+++ SwiftGit2Tests/FileChangesTests.swift
@@ -12,17 +12,6 @@ class FileChangesTests: XCTestCase {
         
         let filePath = "fileA.txt"
         
-        // заважко будувати результат з блобів - треба вручну задавати шляхи і легко напартачити десь
-//        let allCommits = repo1.commitsFromHead() .shouldSucceed("repo1 clone")!
-//        let pairs = allCommits.map { commit in
-//                commit
-//                    .tree()
-//                    .flatMap{ $0.entry(byPath: filePath) }
-//                    .map { blob -> (Commit, Blob) in (commit, blob) }
-//                    //.flatMapError{ blob -> R<(Commit, Blob)?> in .success(nil) }
-//                    //.shouldSucceed("repo1 clone")!
-//            }
-        
         let allCommitsR = repo1.commitsFromHead()
         
         _ = allCommitsR.shouldSucceed("allCommitsR found")!
@@ -38,6 +27,7 @@ class FileChangesTests: XCTestCase {
         
         _ = diffsR.shouldSucceed("diffsR found")!
         
+        
         let changedsOfFileR = combine(allCommitsR, diffsR).map { commits, diffs -> Zip2Sequence<[Commit], [[Diff.Delta]]> in
             zip(commits, diffs)
         }
@@ -52,5 +42,58 @@ class FileChangesTests: XCTestCase {
         print("commits with file changes FOUND: \(changedsOfFile.count)")
         print("-----------------------------------------------")
         print("\r\r\r\r")
+    }
+    
+    func test_changes2() {
+        let folder = root.sub(folder: "Changes")
+        let repo1 = folder.with(repo: "repo1", content: .clone(PublicTestRepo().urlSsh, CloneOptions(fetch: FetchOptions(auth: .credentials(.sshDefault))))).repo.shouldSucceed("repo1 clone")!
+        
+        let filePath = "fileA.txt"
+        
+        let repoId = RepoID(path: "\(folder.url.path)/repo1")
+        
+        print(repoId.path)
+        
+        let a = repoId
+            .getHistoryOfFile(withPath: filePath)
+            .shouldSucceed("changedsOfFileR found")!
+        
+        XCTAssertTrue(a.count > 0, "result history item is MORE than 0 ")
+    }
+}
+
+struct HistoryFileID {
+   let path: String
+   let commitOid: OID // CommitOID
+}
+
+extension HistoryFileID {
+    func getFileContent() -> [Diff.Delta] {
+        return .wtf("Not implemented")
+    }
+}
+
+extension RepoID {
+    func getHistoryOfFile(withPath filePath: String) -> R<[HistoryFileID]> {
+        let repoID = self
+        
+        let allCommitsR = repoID.repo.flatMap { $0.commitsFromHead() }
+        
+        let diffsR = allCommitsR.flatMap { allCommits in
+            allCommits.map { commit in
+                repoID.repo.flatMap{ $0.deltas(target: .commit(commit.oid)) }
+            }
+            .flatMap{ $0 }
+            .map { $0.map { $0.deltasWithHunks } }
+        }
+        .map{ $0.map{ $0.filter { $0.newFile?.path == filePath || $0.oldFile?.path == filePath  } } }
+        
+        let changedsOfFileR = combine(allCommitsR, diffsR)
+            .map { commits, diffs -> Zip2Sequence<[Commit], [[Diff.Delta]]> in
+                zip(commits, diffs)
+            }
+            .map { $0.filter{ $0.1.count > 0 } }
+        
+        return changedsOfFileR.map { $0.map{ HistoryFileID(path: $0.1.first?.newFile?.path ?? $0.1.first?.oldFile?.path ?? "" , commitOid: $0.0.oid ) } }
     }
 }
