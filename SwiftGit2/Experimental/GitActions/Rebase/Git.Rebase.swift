2ed5f326cd19315d887c5cd56d0f34ddaad96b75
Loki <sergiy@vynnychenko.com>
2022-09-06 00:20:04 +0300

    RebaseOptions

--- SwiftGit2/Experimental/GitActions/Rebase/Git.Rebase.swift
+++ SwiftGit2/Experimental/GitActions/Rebase/Git.Rebase.swift
@@ -14,7 +14,9 @@ extension GitRebase {
 extension Repository {
     func rebaseInit(branch: AnnotatedCommit, upstream: AnnotatedCommit, onto: AnnotatedCommit, options: RebaseOptions) -> R<Rebase> {
         git_instance(of: Rebase.self, "git_rebase_init") { pointer in
-            git_rebase_init(&pointer,self.pointer,branch.pointer, upstream.pointer, onto.pointer,&options.options)
+            options.with_git_rebase_options { opt in
+                git_rebase_init(&pointer, self.pointer, branch.pointer, upstream.pointer, onto.pointer, &opt)
+            }
         }
     }
 }
--- SwiftGit2/Experimental/GitActions/Rebase/RebaseOptions.swift
+++ SwiftGit2/Experimental/GitActions/Rebase/RebaseOptions.swift
@@ -2,19 +2,25 @@ import Foundation
 import Essentials
 import Clibgit2
 
-class RebaseOptions {
-    var options = git_rebase_options()
-    let merge : MergeOptions
+public class RebaseOptions {
+    var options     = git_rebase_options()
+    let merge       : MergeOptions
+    let checkout    : CheckoutOptions
     
-    init(merge: MergeOptions) {
+    init(merge: MergeOptions, checkout: CheckoutOptions) {
         self.merge = merge
+        self.checkout = checkout
         git_rebase_options_init(&options, UInt32(GIT_REBASE_OPTIONS_VERSION))
-        options.merge_options = merge.merge_options
+        
+        options.merge_options = merge.merge_options // merge options can be copied as is
     }
 }
 
 extension RebaseOptions {
     func with_git_rebase_options<T>(_ body: (inout git_rebase_options) -> T) -> T {
-        body(&options)
+        checkout.with_git_checkout_options { ch_opt in
+            self.options.checkout_options = ch_opt
+            return body(&options)
+        }
     }
 }
