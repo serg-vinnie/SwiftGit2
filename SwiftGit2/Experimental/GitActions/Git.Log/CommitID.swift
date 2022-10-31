e07fef0bb1afec8fb5fb842fa1f394fa22902abb
Sergiy <sergiy@vynnychenko.com>
2022-10-31 20:39:47 +0200

    Git.CommitInfo

--- SwiftGit2/Experimental/GitActions/Git.Log/CommitID.swift
+++ SwiftGit2/Experimental/GitActions/Git.Log/CommitID.swift
@@ -6,6 +6,8 @@ public struct CommitID {
     let repoID: RepoID
     let oid   : OID
     
+    var commit : R<Commit> { self.repoID.repo | { $0.commit(oid: self.oid) } }
+    
     public init(repoID: RepoID, oid: OID) {
         self.repoID = repoID
         self.oid = oid
--- SwiftGit2/Experimental/GitActions/Git.Log/Git.CommitInfo.swift
+++ SwiftGit2/Experimental/GitActions/Git.Log/Git.CommitInfo.swift
@@ -1,26 +1,33 @@
 import Foundation
 import Clibgit2
+import Essentials
 
 public struct GitCommitInfo {
     let id : CommitID
     let deltas : CommitDeltas
 }
 
+extension CommitID {
+    
+    var basicInfo : R<GitCommitBasicInfo> {
+        let treeOID = commit | { $0.tree() } | { $0.oid }
+        let parents = commit | { $0.parents() } | { $0.map { $0.oid } }
+        return combine(commit,treeOID,parents)
+            | { GitCommitBasicInfo(id: self, commit: $0, tree: $1, parents: $2) }
+    }
+}
+
 public struct GitCommitBasicInfo {
     let id : CommitID
     let author : GitSignature
     let commiter : GitSignature
+    let tree : OID
     
-    init(id: CommitID, author: GitSignature, commiter: GitSignature) {
-        self.id = id
-        self.author = author
-        self.commiter = commiter
-    }
-    
-    init(id: CommitID, commit: Commit) {
+    init(id: CommitID, commit: Commit, tree: OID, parents: [OID]) {
         self.id         = id
         self.author     = GitSignature(commit.author)
         self.commiter   = GitSignature(commit.commiter)
+        self.tree       = tree
     }
 }
 
