d3f9fd200e117410d1fa1e3c1ea424bdda7f63b3
Loki <sergiy@vynnychenko.com>
2022-11-05 18:22:53 +0200

    GitRebaseTests

--- SwiftGit2/Experimental/GitActions/Git.Reference/Git.Reference.swift
+++ SwiftGit2/Experimental/GitActions/Git.Reference/Git.Reference.swift
@@ -11,29 +11,29 @@ public struct GitReference {
     }
 }
 
-extension RepoID {
+public extension RepoID {
     enum HeadType {
         case attached(ReferenceID)
-        case detached(OID)
+        case detached(OID, RepoID)
         
-        var asReference : R<ReferenceID> {
+        public var asReference : ReferenceID {
             switch self {
-            case .attached(let ref): return .success(ref)
-            case .detached(_): return .wtf("head is detacched")
+            case .attached(let ref):        return ref
+            case .detached(_, let repoID):  return ReferenceID(repoID: repoID, name: "HEAD")
             }
         }
         
-        var asOID : R<OID> {
+        public var asOID : R<OID> {
             switch self {
             case .attached(let ref): return ref.targetOID
-            case .detached(let oid): return .success(oid)
+            case .detached(let oid, _): return .success(oid)
             }
         }
     }
 
     var HEAD : R<HeadType> {
         self.repo.if(\.headIsDetached,
-                      then: { repo in repo.HEAD() | { Duo($0, repo).targetOID() } | { HeadType.detached($0)} },
+                      then: { repo in repo.HEAD() | { Duo($0, repo).targetOID() } | { HeadType.detached($0, self)} },
                       else: { repo in repo.HEAD() | { HeadType.attached(ReferenceID(repoID: self, name: $0.nameAsReference))} }
         )
     }
--- SwiftGit2Tests/GitRebaseTests.swift
+++ SwiftGit2Tests/GitRebaseTests.swift
@@ -6,6 +6,27 @@ import EssetialTesting
 class GitRebaseTests: XCTestCase {
     let root = TestFolder.git_tests.sub(folder: "GitRebaseTests")
     
+    func test_HEAD() {
+        let repoID = root.with(repo: "head", content: .commit(.fileA, .content1, "1")).shouldSucceed()!.repoID
+        
+        let oid_1 = repoID.HEAD
+            .shouldSucceed("HEAD")!
+            .asReference        // ReferenceID(repoID: RepoID /Users/loki/.git_tests/GitRebaseTests/head, name: "refs/heads/main")
+            .targetOID
+            .shouldSucceed()!
+            
+        (repoID.repo | { $0.detachHEAD() })
+            .shouldSucceed("detachHEAD")
+        
+        let oid_2 = repoID.HEAD
+            .shouldSucceed("HEAD")!
+            .asReference    // ReferenceID(repoID: RepoID /Users/loki/.git_tests/GitRebaseTests/head, name: "HEAD")
+            .targetOID
+            .shouldSucceed()!
+            
+        XCTAssertEqual(oid_1, oid_2)
+    }
+    
     func test_rebaseFastForward() {
         // create repo with intial commit
         // branch
