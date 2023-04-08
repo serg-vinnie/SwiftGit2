346b595c55c9228bedebbfcfd91a1f0fe2149e2d
Loki <sergiy@vynnychenko.com>
2023-04-08 12:50:14 +0300

    Repository+Blob

--- SwiftGit2/Experimental/Instance/Repository/Repository+Blob.swift
+++ SwiftGit2/Experimental/Instance/Repository/Repository+Blob.swift
@@ -1,10 +1,3 @@
-//
-//  Repository+Blob.swift
-//  SwiftGit2-OSX
-//
-//  Created by UKS on 25.07.2021.
-//  Copyright © 2021 GitHub, Inc. All rights reserved.
-//
 
 import Foundation
 import Clibgit2
@@ -15,26 +8,18 @@ public extension Repository {
     func blobCreateFromDisk(path: String) -> R<OID> {
         var oid = git_oid()
         
-        return _result({ OID(oid) }, pointOfFailure: "git_blob_create_from_disk") {
-            path.withCString{ path1 in
-                Clibgit2.git_blob_create_from_disk(&oid, self.pointer, path1)
-            }
+        return git_try("git_blob_create_from_disk") {
+            git_blob_create_from_disk(&oid, self.pointer, path)
         }
+        | { _ in OID(oid) }
     }
     
-    func blobCreateFromWorkdir(relPath: String) -> R<OID> {
+    func blobCreateFromWorkdir(path: String) -> R<OID> {
         var oid = git_oid()
         
-        return _result({ OID(oid) }, pointOfFailure: "git_blob_create_from_workdir") {
-            relPath.withCString{ path1 in
-                Clibgit2.git_blob_create_from_workdir(&oid, self.pointer, path1)
-            }
+        return git_try("git_blob_create_from_workdir") {
+            git_blob_create_from_workdir(&oid, self.pointer, path)
         }
-    }
-    
-    func blobCreateFromWorkdirAsBlob(relPath: String) -> R<Blob> {
-        let repo = self
-        
-        return blobCreateFromWorkdir(relPath: relPath).flatMap{ repo.blob(oid: $0) }
+        | { _ in OID(oid) }
     }
 }
