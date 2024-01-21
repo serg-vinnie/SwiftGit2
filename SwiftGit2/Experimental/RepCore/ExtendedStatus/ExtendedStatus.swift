4bcb76641c9b23b764958e5587311e6006030f51
Loki <sergiy@vynnychenko.com>
2024-01-21 02:18:22 +0200

    ExtendedStatus .statusID   : UUID

--- SwiftGit2/Experimental/RepCore/ExtendedStatus/ExtendedStatus.swift
+++ SwiftGit2/Experimental/RepCore/ExtendedStatus/ExtendedStatus.swift
@@ -2,14 +2,16 @@
 import Clibgit2
 import Essentials
 
-public struct StatusEntryID : Identifiable {
-    public let repoID : RepoID
-    public let idx : Int
+public struct StatusEntryID : Identifiable, Hashable {
+    public let repoID     : RepoID
+    public let statusID   : UUID
+    public let idx        : Int
     
-    public var id: String { repoID.path + "_\(idx)" }
+    public var id: String { repoID.path + statusID.uuidString + "_\(idx)" }
     
-    public init(repoID: RepoID, idx: Int) {
+    public init(repoID: RepoID, statusID: UUID, idx: Int) {
         self.repoID = repoID
+        self.statusID = statusID
         self.idx = idx
     }
 }
