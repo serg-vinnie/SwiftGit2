81bf29aec61d2b533c2e659a8b9633b0c7cc3a56
Sergiy <sergiy@vynnychenko.com>
2024-01-25 12:38:36 +0200

    StatusEntryIdx

--- SwiftGit2/Experimental/RepCore/ExtendedStatus/StatusEntryIdx.swift
+++ SwiftGit2/Experimental/RepCore/ExtendedStatus/StatusEntryIdx.swift
@@ -6,12 +6,14 @@ public struct StatusEntryIdx : Identifiable, Hashable {
     public let repoID     : RepoID
     public let statusID   : UUID
     public let idx        : Int
+    public let stagePath  : String
     
-    public var id: String { repoID.path + statusID.uuidString + "_\(idx)" }
+    public var id: String { repoID.path + ":" + stagePath + "_\(idx)@" + statusID.uuidString }
     
-    public init(repoID: RepoID, statusID: UUID, idx: Int) {
+    public init(repoID: RepoID, statusID: UUID, idx: Int, stagePath: String) {
         self.repoID = repoID
         self.statusID = statusID
         self.idx = idx
+        self.stagePath = stagePath
     }
 }
