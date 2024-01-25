9e478eef03bdda20a0d2751fec2394c2bfefd489
Loki <sergiy@vynnychenko.com>
2024-01-25 03:12:41 +0200

    ExtendedStatus.swift

--- SwiftGit2/Experimental/RepCore/ExtendedStatus/ExtendedStatus.swift
+++ SwiftGit2/Experimental/RepCore/ExtendedStatus/ExtendedStatus.swift
@@ -11,7 +11,20 @@ public struct ExtendedStatus {
     public let status : StatusIterator
     public let isConflicted : Bool
     public let head: HEAD
-    public let uuid = UUID()
+    public let uuid : UUID
+    
+    init(status: StatusIterator, isConflicted: Bool, head: HEAD, uuid: UUID = UUID()) {
+        self.status = status
+        self.isConflicted = isConflicted
+        self.head = head
+        self.uuid = uuid
+    }
     
     public static var empty : ExtendedStatus { ExtendedStatus(status: StatusIterator(nil), isConflicted: false, head: .isUnborn) }
+}
+
+public extension ExtendedStatus {
+    func replacing(uuid: UUID) -> ExtendedStatus {
+        ExtendedStatus(status: self.status, isConflicted: self.isConflicted, head: self.head, uuid: uuid)
+    }
 }
