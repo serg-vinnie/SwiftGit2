7815b407769974b78abd4ffd333eaa59cb05a753
Loki <sergiy@vynnychenko.com>
2024-03-08 15:37:13 +0200

    TreeID.Cache.swift WIP

--- SwiftGit2/Experimental/GitDB/TreeID.Cache.swift
+++ SwiftGit2/Experimental/GitDB/TreeID.Cache.swift
@@ -8,7 +8,7 @@ public extension TreeID {
     }
     
     var cacheTrees : R<TreeID.Cache> {
-        entries | { $0.compactMap { $0.asCacheTree.maybeSuccess } } | { TreeID.Cache(tree: self, entries: $0) }
+        entries | { $0.compactMap { $0.asCacheTree(path: nil).maybeSuccess } } | { TreeID.Cache(tree: self, entries: $0) }
     }
     
     struct Cache {
@@ -23,15 +23,15 @@ public extension TreeID.Entry {
     
     fileprivate func asCacheEntry(path: String? = nil) -> R<TreeID.Cache.Entry> {
         if self.kind == .tree {
-            return children | { $0 | { $0.asCacheEntry() } } | { TreeID.Cache.Entry(oid: self.oid, name: self.name, children: $0) }
+            return children | { $0 | { $0.asCacheEntry() } } | { TreeID.Cache.Entry(oid: self.oid, name: self.name, path: path, children: $0) }
         }
         
-        return .success(TreeID.Cache.Entry(oid: self.oid, name: self.name, children: nil))
+        return .success(TreeID.Cache.Entry(oid: self.oid, name: self.name, path: path, children: nil))
     }
     
-    var asCacheTree : R<TreeID.Cache.Entry> {
+    func asCacheTree(path: String? = nil) -> R<TreeID.Cache.Entry> {
         if self.kind == .tree {
-            return children | { $0.compactMap { $0.asCacheTree.maybeSuccess } } | { TreeID.Cache.Entry(oid: self.oid, name: self.name, children: $0) }
+            return children | { $0.compactMap { $0.asCacheTree(path: path).maybeSuccess } } | { TreeID.Cache.Entry(oid: self.oid, name: self.name, children: $0) }
         }
         
         return .wtf("not a tree")
@@ -42,11 +42,26 @@ public extension TreeID.Cache {
     struct Entry : Identifiable {
         public let oid : OID
         public let name : String
-//        public let path : String
+        public let path : String?
         public let children : [TreeID.Cache.Entry]?
         
         public var id : OID { oid }
         
-        
+
+        init(oid: OID, name: String, path: String? = nil, children: [TreeID.Cache.Entry]?) {
+            self.oid = oid
+            self.name = name
+            self.path = path
+            self.children = children
+        }
+    }
+}
+
+extension String {
+    func appendingPath(component: String) -> String {
+        if self.hasSuffix("/") {
+            return self + component.trimStart("/")
+        }
+        return self + "/" + component.trimStart("/")
     }
 }
