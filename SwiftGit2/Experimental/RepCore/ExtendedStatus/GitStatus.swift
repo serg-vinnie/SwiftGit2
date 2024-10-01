7e019baac4d5326ea47329ae2f6579f9c3329df5
Loki <sergiy@vynnychenko.com>
2024-10-02 01:21:13 +0300

    GitStatus

--- SwiftGit2/Experimental/RepCore/ExtendedStatus/GitStatus.swift
+++ SwiftGit2/Experimental/RepCore/ExtendedStatus/GitStatus.swift
@@ -20,9 +20,20 @@ extension GitStatus {
         repoID.repo | { $0.extendedStatus() } | { $0.createStorage(repoID: self.repoID).updatingCache().status }
     }
     
-    func refreshingPartial(options: StatusOptions = StatusOptions()) -> R<ExtendedStatus> {
-//        repoID.repo | { $0.extendedStatus() } | { $0.createStorage(repoID: self.repoID).updatingCache().status }
-        return .notImplemented
+    func refreshingPartial(statusEx newStatus: ExtendedStatus) -> R<ExtendedStatus> {
+        guard let storage = _storage[repoID] else { return .wtf("storage not found for \(repoID)") }
+        let oldStatusEx = storage.status
+        let oldSignature = oldStatusEx.signature
+        let newSignature = newStatus.signature
+        
+        if oldSignature == newSignature {
+            let newS = newStatus.replacing(uuid: oldStatusEx.uuid)
+            _storage[repoID] = StatusStorage(status: newS, repoID: repoID)
+            return .success(newS)
+        } else {
+            _storage[repoID] = StatusStorage(status: newStatus, repoID: repoID)
+            return .success(newStatus)
+        }
     }
 }
 
