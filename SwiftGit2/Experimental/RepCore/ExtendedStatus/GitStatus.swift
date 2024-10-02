32c0f5e68d54c041ae923910483b08de78e46537
Loki <sergiy@vynnychenko.com>
2024-10-03 01:50:30 +0300

    ExtendedStatus.stagingRatio

--- SwiftGit2/Experimental/RepCore/ExtendedStatus/GitStatus.swift
+++ SwiftGit2/Experimental/RepCore/ExtendedStatus/GitStatus.swift
@@ -26,8 +26,42 @@ public extension GitStatus {
     func refreshingSoft() -> R<ExtendedStatus> {
         storage.refreshingSoft()
     }
+}
+
+public extension ExtendedStatus {
+    var stagingRatio : CGFloat {
+        let state = status.map { $0.stageState }
+        
+        let staged = state.filter { $0 == .staged }
+        let unstaged = state.filter { $0 == .unstaged }
+        
+        if staged.count == state.count {
+            return 1
+        }
+        
+        if unstaged.count == state.count {
+            return 0
+        }
+        
+        let weight = state.map { $0.weight }.reduce(0, +) / CGFloat(state.count)
+        return clamp(weight, min: 0.1, max: 0.9)
+    }
+}
+
+fileprivate func clamp<T: Comparable>(_ val: T, min: T, max: T) -> T {
     
-//    var statusEx : R<ExtendedStatus> {
-//        storage | { $0.statusEx }
-//    }
+    if val < min { return min }
+    if val > max { return max }
+    return val
+}
+
+extension StageState {
+    var weight : CGFloat {
+        switch self {
+        case .staged: 1
+        case .unstaged: 0
+        case .mixed: 0.5
+        case .unavailable: 0
+        }
+    }
 }
